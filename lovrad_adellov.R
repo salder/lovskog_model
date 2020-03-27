c3c08c950757e0b4d84f0735cc5fb6f83932aaff
Sven Adler <sven.adler@slu.se>
2020-03-27 15:18:31 +0100

    october scene

--- lovrad_adellov.R
+++ lovrad_adellov.R
@@ -214,6 +214,116 @@ saveRDS(taxdata.m,file="F:/Lovtrad_model/model_data_lov_adel_aug.rds")
 
 
 
+#################################################################################################################################################
+#october scene
+
+
+library(googledrive)
+drive_find()    #choose "2"
+setwd("L:/Lovtrad_model/GEE")
+
+t1<-drive_find()
+files1<-grep(c("adellov_host"),t1$name,value=TRUE)
+
+
+
+for (i in 1:length(files1))
+  drive_download(file=files1[i],overwrite=TRUE)
+
+
+
+filelist_adlov<-dir(path="L:/Lovtrad_model/GEE",pattern="adellov_aug",full.names = TRUE)
+
+
+#NFI data
+taxdata<-readRDS("F:/Lovtrad_model/model_data_lov_adel_aug.rds")                
+taxdata<-taxdata %>% filter(DelytaNr==0) %>% filter(Taxar>2013)
+
+taxdata.sp<-SpatialPointsDataFrame(coords=taxdata[,c("Ostkoordinat","Nordkoordinat")],data=taxdata,proj4string=CRS(projSWEREF))
+
+
+
+
+#tif files have more than one layer -> multilayer -> the band needt to chose!
+
+tile.extract.multilayer<-function(file_list=files1,data.sp=taxdata.sp,bandnr=1)
+{
+  train_m<-NA
+  #ndvi1
+  for (i in 1:length(file_list))
+  {
+    #i=1
+    ras.t<-raster(file_list[i],band=bandnr)                   #.t for "tiles"
+    e.t<-extent(ras.t)
+    train.sp<-crop(data.sp,e.t)
+    if(is.null(train.sp)==FALSE)
+    {
+      train<-train.sp@data
+      train$extract<-extract(ras.t,train.sp)
+      train_m<-rbind(train_m,train)
+    }
+  }
+  train.res<-train_m %>% dplyr::select(Ostkoordinat,Nordkoordinat,extract)
+  var.name=paste("band_oct",bandnr,sep="")
+  names(train.res)[[3]]<-var.name
+  print(train.res)
+}
+
+
+dat.band1<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=1)
+dat.band2<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=2)
+dat.band3<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=3)
+dat.band4<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=4)
+dat.band5<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=5)
+dat.band6<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=6)
+dat.band7<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=7)
+dat.band8<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=8)
+dat.band9<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=9)
+dat.band10<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=10)
+dat.band11<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=11)
+dat.band12<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=12)
+#dat.band13<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=13)
+#dat.band14<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=14)
+#dat.band15<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=15)
+#dat.band16<-tile.extract.multilayer(file_list=filelist_adlov,data=taxdata.sp,bandnr=16)
+
+
+taxdata.m<-taxdata %>% left_join(.,dat.band1,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band2,by=c("Ostkoordinat","Nordkoordinat"))%>% 
+  left_join(.,dat.band3,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band4,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band5,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band6,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band7,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band8,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band9,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band10,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band11,by=c("Ostkoordinat","Nordkoordinat")) %>% 
+  left_join(.,dat.band12,by=c("Ostkoordinat","Nordkoordinat")) 
+
+saveRDS(taxdata.m,file="F:/Lovtrad_model/model_data_lov_adel_aug_okt.rds")
+
+#merge with lovtrad model
+
+lovtrad.r<-raster("L:/Lovtrad_model/lovtrad_sverige_sanolikhet_0_1.tif")
+
+taxdata.m.sp<-SpatialPointsDataFrame(coords=taxdata.m[,c("Ostkoordinat","Nordkoordinat")],data=taxdata.m,proj4string=CRS(projSWEREF))
+
+taxdata.m$lovtrad.m<-extract(lovtrad.r,taxdata.m.sp)
+saveRDS(taxdata.m,file="F:/Lovtrad_model/model_data_lov_adel_aug_okt.rds")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -284,6 +394,10 @@ fit.adel<-gam(form,data=trainingsdata,"quasibinomial")
 summary(fit.adel)
 
 
+source("drop_cont.R")
+#time consuming!! run just one time with the final model!
+res<-drop_cont(form=form,data_use=trainingsdata,fam="quasibinomial")
+
 
 #alternativt modell
 
@@ -349,14 +463,16 @@ test.res1<-testdata %>% mutate(adellov=ifelse(adelandel/100>0.5,1,0)) %>%
   filter(adellov==1)
 
 
-boxplot(test.res1$pred.adel~test.res1$adellov.pre)
 
 
 table(test.res1$pred.error)
 prop.table(table(test.res1$pred.error))
 
-t(data.frame(test.res1[13,]))
+View(test.res1)
 
+t(data.frame(test.res1[14,]))
+
+boxplot(test.res1$Ekandel~test.res1$adellov.pre)
 
 
 
